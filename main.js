6f9dcb0a15f4129ca2410c38e827cc0044704fd9
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-20 11:14:43 +0200

    jobs and skills in

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 import './style.scss';
 
 const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
+const skillsUrl = 'https://edwardtanguay.vercel.app/share/skills.json';
 
 (async () => {
 	const jobs = (await axios.get(jobsUrl)).data;
+	const skills = (await axios.get(skillsUrl)).data;
 
 	document.querySelector('#app').innerHTML = `
   <div>
