3d406947542de5b7277b404f4ae8da5fd28aaa3d
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-20 11:36:34 +0200

    showing 5 skills

--- main.js
+++ main.js
@@ -1,26 +1,44 @@
 import axios from 'axios';
 import './style.scss';
 
+const config = {
+	numberOfQuestions: 5
+};
+
 const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
 const skillsUrl = 'https://edwardtanguay.vercel.app/share/skills.json';
 
-(async () => {
-	const jobs = (await axios.get(jobsUrl)).data;
-	const skills = (await axios.get(skillsUrl)).data;
-
-  const displayQuizHtml = () => {
-    let html = '<div class="quizArea">';
-    for (const skill of skills) {
-        html += `
+const displaySkill = (skill) => {
+	let html = '';
+	html += `
 <div class="skill">
     <div class="name">${skill.name}</div>
     <div class="description">${skill.description}</div>
 </div>
 `;
-    }
-html += '</div>';
-    return html;
+	return html;
 };
+
+(async () => {
+	const jobs = (await axios.get(jobsUrl)).data;
+	const skills = (await axios.get(skillsUrl)).data;
+
+	const getQuizSkills = () => {
+		const randomSkills = [];
+		for (let i = 0; i < config.numberOfQuestions; i++) {
+			randomSkills.push(skills[i]);
+		}
+		return randomSkills;
+	};
+
+	const displayQuizHtml = () => {
+		let html = '<div class="quizArea">';
+		for (const skill of getQuizSkills()) {
+			html += displaySkill(skill);
+		}
+		html += '</div>';
+		return html;
+	};
 
 	document.querySelector('#app').innerHTML = `
   <div>
