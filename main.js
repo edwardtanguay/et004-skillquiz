3c9d65498771e7ddefaf60650f14576e701d7786
Edward Tanguay (windows) <edward@tanguay.info>
2023-04-20 11:09:08 +0200

    axios one-liner

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-import './style.scss'
+import axios from 'axios';
+import './style.scss';
+
+const jobsUrl = 'https://edwardtanguay.vercel.app/share/jobs.json';
+
+const jobs = (await axios.get(jobsUrl)).data;
 
 document.querySelector('#app').innerHTML = `
   <div>
   <h1>Webdev Skill Quiz</h1>
   <p>Welcome to this site.</p>
   </div>
-`
+`;
